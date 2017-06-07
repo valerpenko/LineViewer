5d5a7f4f5dfee0c87af1c4288934ec4926188dc4
valerpenko <vpenko@onu.edu.ua>
2017-06-07 11:12:43 +0300

    Drawing 1 line

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -6,14 +6,22 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using LineViewerLibrary;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private Line line;
         public Form1()
         {
+            line=new Line(20.5, 30.0, 40.0, 10.5);
             InitializeComponent();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Pens.Black, line.P1(), line.P2());
         }
     }
 }
