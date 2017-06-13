ecdda331b01bcd3dc7339e16067121ad28cc7e1d
valerpenko <vpenko@onu.edu.ua>
2017-06-13 19:46:01 +0300

    lines added and repainted when added"
    e

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,17 +11,33 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private Line line;
+        private List<Line> lines;
         public Form1()
         {
-            line=new Line(20.5, 30.0, 40.0, 10.5);
+            lines = new List<Line>();
+            lines.Add(new Line(20.5, 30.0, 40.0, 10.5));
             InitializeComponent();
 
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawLine(Pens.Black, line.P1(), line.P2());
+            for (int i = 0;i<lines.Count;i++)
+            e.Graphics.DrawLine(Pens.Black, lines[i].P1(), lines[i].P2());
+           //foreach(Line x in lines)
+            //e.Graphics.DrawLine(Pens.Black, x.P1(), x.P2());
+
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1 - add new line
+            Random rnd = new Random();
+            lines.Add(new Line(rnd.NextDouble()*100.0, rnd.NextDouble() * 100.0, rnd.NextDouble() * 100.0, rnd.NextDouble() * 100.0));
+            //2 - draw this line (old lines are drawn)
+            this.Refresh();
+        }
+
     }
 }
+
