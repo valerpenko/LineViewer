2d8aff1380d41673ffd76108ec2d24ff25fe93f3
valerpenko <vpenko@onu.edu.ua>
2017-06-28 19:34:20 +0300

    линии с цветами КОЖЗ добавить остальные

---
+++ LineViewerLibrary/Line.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace LineViewerLibrary
+{
+    public class Line
+    {
+        private double x1, y1;
+        private double x2, y2;
+        private Pen color;
+        public Line(double px1, double py1, double x2, double y2)
+        {
+            x1 = px1;
+            y1 = py1;
+            this.x2 = x2;
+            this.y2 = y2;
+            Random r = new Random();
+            int c = r.Next(0, 7);
+            switch (c)
+            {
+                case 0: color = Pens.Red; break;
+                case 1: color = Pens.Orange; break;
+                case 2: color = Pens.Yellow; break;
+                default: color = Pens.Green; break;
+            }
+        }
+        public PointF P1()
+        {
+            return new PointF(Convert.ToSingle(x1), Convert.ToSingle(y1));
+        }
+        public PointF P2()
+        {
+            return new PointF(Convert.ToSingle(x2), Convert.ToSingle(y2));
+        }
+        public Pen Color()
+        {
+            return color;
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -32,8 +32,9 @@ namespace WindowsFormsApp1
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            for (int i = 0;i<lines.Count;i++)
-                e.Graphics.DrawLine(Pens.Black, lines[i].P1(), lines[i].P2());
+            for (int i = 0; i < lines.Count; i++)
+            { e.Graphics.DrawLine(lines[i].Color(), lines[i].P1(), lines[i].P2());
+            }
                 //foreach(Line x in lines)
                 //e.Graphics.DrawLine(Pens.Black, x.P1(), x.P2());
         }
